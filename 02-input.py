5ff3a7b939c1e05ce50a3e69bf45fba7cf55b17e
Adarsh Patel <57874420+panda7ad@users.noreply.github.com>
2026-02-27 00:36:34 +0530

    Fix input handling and improve code comments
    Corrected input handling and added comments for clarity.

--- 02-input.py
+++ 02-input.py
@@ -1,6 +1,7 @@
+#Taking Input from user
 a=(input("Enter a number 1:"))
 b=(input("Enter a number 2:"))
-#
+#Displaying Input And Result
 print("Your FIRST NUMBER is:",a)
 print("Your SECOND Number is:",b)
 print("Sum of BOTH NUMBER is :",a+b)
